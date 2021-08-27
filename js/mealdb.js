af77016414c3ec006dd4bb246cd47a82894be540
avishekataur <avishekataur@gmail.com>
2021-08-27 21:52:25 +0600

    meal-finished

--- js/mealdb.js
+++ js/mealdb.js
@@ -24,7 +24,7 @@ const displaySearchedMeal = meals =>
         div.classList.add('col');
         div.innerHTML=
         `
-        <div onclick = "loadMealDetail(${meal.idMeal})" class="card">
+        <div onclick="loadMealDetail(${meal.idMeal})" class="card">
         <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
         <div class="card-body">
           <h5 class="card-title">${meal.strMeal}</h5>
@@ -47,10 +47,26 @@ const loadMealDetail = mealID =>
     ` ;
     fetch(url)
   .then(response => response.json())
-  .then(data => console.log(data.meals[0]))
+  .then(data => displayMealDetail(data.meals[0]))
 }
 
 const displayMealDetail = meal =>
 {
-    console.log(meal);
+    // console.log(meal);
+    const singleMealDetails = document.getElementById('single-mealDetails');
+    const divDetail = document.createElement('div');
+    divDetail.classList.add('card');
+    divDetail.innerHTML =
+    
+    `
+    <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+
+    <div class="card-body">
+          <h5 class="card-title">${meal.strMeal}</h5>
+          <p class="card-text">${meal.strInstructions.slice(0, 100)}</p>
+          <a href="${meal.strYoutube}" class="btn btn-danger">Watch</a>
+    </div>
+
+    `;
+    singleMealDetails.appendChild(divDetail);
 }
