bd818897ca2802092b73dd07876c3424129db178
avishekataur <avishekataur@gmail.com>
2021-08-28 18:51:29 +0600

    random-users-done

--- js/random-users.js
+++ js/random-users.js
@@ -8,20 +8,28 @@ const users = () =>
 const allUsers = (data) =>
 {
     const fiveUsers = data.results;
-    const div = document.getElementById('users');
+    const divUsers = document.getElementById('users');
     for(const user of fiveUsers)
     {
         // console.log(user.email);
-        const p = document.createElement('p');
+        const div = document.createElement('div');
         // p.innerText = user.name;
-        p.classList.add('p')
-        p.innerText = `
-        Name: ${user.name.title} ${user.name.first} ${user.name.last}
-        Email: ${user.email}
-        Gender: ${user.gender}
-        Location: ${user.location.city}
+        div.classList.add('users')
+        div.innerHTML = `
+
+        <div>
+
+        <img style="width: 280px; height: 280px;" src="${user.picture.large}" >
+                
+        <p>Name: ${user.name.title} ${user.name.first} ${user.name.last}</p>
+        <p>Email: ${user.email}</p>
+        <p>Gender: ${user.gender}</p>
+        <p>Location: ${user.location.city}</p>
+        
+        </div>
+        
         `;
-        div.appendChild(p);
+        divUsers.appendChild(div);
     }
 }
 
